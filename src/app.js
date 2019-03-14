5e73ffdb03eda5ed926a9efeafb739c8637e12f5
Alexandre ACP <alexandrepalagem@cwi.com.br>
2019-03-14 03:11:00 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  SafeAreaView
+} from "react-native";
+
+import api from "./api/feed.json";
+
+import { Post } from "./components/post.component";
+
+class App extends Component {
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+        {this._renderNavBar()}
+        <ScrollView>
+          {api.feed.map((post, index) => (
+            <Post post={post} key={index} />
+          ))}
+        </ScrollView>
+        {this._renderTabs()}
+      </SafeAreaView>
+    );
+  }
+
+  _renderNavBar() {
+    return (
+      <View
+        style={{
+          backgroundColor: "#fff",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          height: 62,
+          paddingHorizontal: 10,
+          paddingTop: 20,
+          borderBottomWidth: 1,
+          borderColor: "#ddd"
+        }}
+      >
+        <Image
+          source={require("./img/camera.png")}
+          style={{ width: 23, height: 23 }}
+        />
+        <Image
+          source={require("./img/instagram_logo.png")}
+          style={{ width: 100, height: 40, resizeMode: "contain" }}
+        />
+        <Image
+          source={require("./img/send.png")}
+          style={{ width: 23, height: 23 }}
+        />
+      </View>
+    );
+  }
+
+  _renderTabs() {
+    return (
+      <View
+        style={{
+          backgroundColor: "#fff",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          height: 42,
+          paddingHorizontal: 30,
+          borderTopWidth: 1,
+          borderColor: "#ddd"
+        }}
+      >
+        <Image
+          source={require("./img/camera.png")}
+          style={{ width: 23, height: 23 }}
+        />
+        <Image
+          source={require("./img/send.png")}
+          style={{ width: 23, height: 23 }}
+        />
+        <Image
+          source={require("./img/camera.png")}
+          style={{ width: 23, height: 23 }}
+        />
+        <Image
+          source={require("./img/send.png")}
+          style={{ width: 23, height: 23 }}
+        />
+        <Image
+          source={require("./img/camera.png")}
+          style={{ width: 23, height: 23 }}
+        />
+      </View>
+    );
+  }
+}
+
+export default App;
+
+const styles = StyleSheet.create({});
