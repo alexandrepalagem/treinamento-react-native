f34ff6794045bc268fc9eca8078fe0ba06645c7a
Pablo Brodt <pablo.brodt@cwi.com.br>
2019-03-22 21:17:10 -0300

    add camera route

---
+++ src/ui/navigator/routes/camera/camera.map.js
@@ -0,0 +1,10 @@
+import { CAMERA_ROUTES } from './camera.route'
+
+// import { CameraScreen } from '@ui/screens/camera'
+import { CameraScreen } from '../../../screens/camera/camera.screen'
+
+export const CameraRoutes = {
+  [CAMERA_ROUTES.CAMERA]: {
+    screen: CameraScreen
+  }
+}
